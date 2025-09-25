719adc08250b1c97a0067cb11bea68c4145ee70e
GreedyAllay <cattiesworlds@gmail.com>
2025-09-25 20:10:21 +0200

    stuff hopefully didnt bweak anything :3

--- resources/extensions/html.js
+++ resources/extensions/html.js
@@ -1,6 +1,8 @@
 (function (Scratch) {
   'use strict';
 
+    if (!Scratch.extensions.unsandboxed) throw new Error("Run it unsandboxed or face the consequences (It just won't work)")
+
   let elements = []
   let IDs = []
   let clicked = {}
--- resources/extensions/javascript.js
+++ resources/extensions/javascript.js
@@ -1,6 +1,8 @@
 (function (Scratch) {
   'use strict';
 
+  
+
   class MyExtension {
     getInfo() {
       return {
